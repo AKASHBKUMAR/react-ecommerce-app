8f5b59ac8d2bffa391634f9e6ebf5c0436d037c0
Akash_Kumar <akashbkumar30@gmail.com>
2024-07-16 08:34:16 +0530

    Final Commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
 import CartProvider from './Context/CartProvider'
 import NavigationComponent from './Components/NavigationComponent/NavigationComponent'
+import HomeComponent from './Components/HomeComponent/HomeComponent';
 
 const App = () => {
     return (
         <CartProvider>
-            <NavigationComponent/>
+            <NavigationComponent />
+            <HomeComponent />
         </CartProvider>
     )
 }
